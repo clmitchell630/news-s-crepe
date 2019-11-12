fb4913e834cfcddc3a2759265e7671951161d35c
clmitchell630 <clmitchell630@gmail.com>
2019-11-12 17:32:50 -0600

    05 new buttons, and a modal. final step, make notes work!

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 $("#scrape").on("click", e => {
     e.preventDefault();
-
     $.ajax("/api/scrape", {
         type: "GET"
     }).then(() => location.reload());
@@ -8,33 +7,44 @@ $("#scrape").on("click", e => {
 
 $("#clear").on("click", e => {
     e.preventDefault();
-
     $.ajax("/api/all/remove", {
         type: "DELETE"
-    }).then(() =>location.reload());
+    }).then(() => location.reload());
 });
 
-$(".save-article").on("click", function(e) {
+$(".save-article").on("click", function (e) {
     e.preventDefault();
-
     const id = $(this).attr("data-id");
     const saved = {
-        saved : true
+        saved: true
     };
-
     $.ajax("/api/update/" + id, {
-        type:"PUT",
+        type: "PUT",
         data: saved
     }).then(() => location.reload());
-
 });
 
-$(".delete-article").on("click", function(e) {
+$(".delete-article").on("click", function (e) {
     e.preventDefault();
-
     const id = $(this).attr("data-id");
-    
     $.ajax("/api/myarticles/remove/" + id, {
-        type:"DELETE"
+        type: "DELETE"
+    }).then(() => location.reload());
+});
+
+$(".note-article").on("click", function (e) {
+    e.preventDefault();
+    $("#add-notes").modal("toggle");
+});
+
+$(".post-notes").on("click", function (e) {
+    e.preventDefault();
+    const id = $(this).attr("data-id");
+    const note = {
+        body: req.body.note
+    }
+    $.ajax("/api/addnote/" + id, {
+        type: "POST",
+        data: note
     }).then(() => location.reload());
 });
